1bf8e9498e90806d0ddb79a29d0ce12cc49d461b
Catarine Tomaz <165063607+tomazzcatarine@users.noreply.github.com>
2025-05-30 16:23:52 -0300

    Update run.py

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-app.run(host='127.0.0.1', port=80, debug=True)
+app.run(host='0.0.0.0', port=80, debug=True)
